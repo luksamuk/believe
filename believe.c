13b0c78c5fb95bf56f47dc267c9123ee9e20611c
Lucas Vieira <lucasvieira@protonmail.com>
2019-10-16 17:30:57 -0300

    Add many changes, see commit details
    - Add section for forward declarations;
    - Add predicates: symbolp, nilp, pairp, atomp, charp, streamp, idp,
      errorp;
    - Add error checks on some functions;
    - Implement and specify error objects as literals;
    - Implement and specify `nil` as both an atom and a pair;
    - Add function bel_char_from_binary to convert a list of eight 0 or 1
      characters into a displayable Bel character;
    - Add a simple test for error objects displaying (WIP).

--- believe.c
+++ believe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <errno.h>
 
 #define GC_DEBUG
 #include <gc.h>
@@ -75,6 +76,53 @@ Bel *bel_g_outs;
 Bel *bel_g_globe;
 Bel *bel_g_scope;
 
+Bel *bel_mkerror(Bel *format, Bel *vars);   // Forward declaration
+Bel *bel_mkstring(const char*);             // Forward declaration
+Bel *bel_mksymbol(const char*);             // Forward declaration
+Bel *bel_car(Bel*);                         // Forward declaration
+Bel *bel_cdr(Bel*);                         // Forward declaration
+
+#define bel_symbolp(x) (x->type==BEL_SYMBOL)
+
+#define bel_nilp(x)                             \
+    (bel_symbolp(x) && (x->sym==BEL_NIL))
+
+#define bel_pairp(x)                            \
+    ((x->type==BEL_PAIR) || bel_nilp(x))
+
+#define bel_atomp(x)                             \
+    (bel_nilp(x) || !bel_pairp(x))
+
+#define bel_charp(x)                            \
+    ((x->type==BEL_CHAR))
+
+#define bel_streamp(x)                          \
+    ((x->type==BEL_STREAM))
+
+int
+bel_idp(Bel *x, Bel *y)
+{
+    if((!bel_symbolp(x) && !bel_charp(x))
+       || (!bel_symbolp(y) && !bel_charp(y)))
+        return 0;
+
+    if(bel_symbolp(x))
+        return (x->sym == y->sym);
+    return (x->chr == y->chr);
+}
+
+int
+bel_errorp(Bel *x)
+{
+    if(!bel_pairp(x)) return 0;
+    if(!bel_idp(bel_car(x), bel_mksymbol("lit")))
+        return 0;
+    Bel *cdr = bel_cdr(x);
+    if(!bel_idp(bel_car(cdr), bel_mksymbol("err")))
+        return 0;
+    return 1;
+}
+
 typedef struct {
     const char **tbl;
     uint64_t     n_syms;
@@ -146,13 +194,15 @@ bel_mkpair(Bel *car, Bel *cdr)
     return ret;
 }
 
-#define bel_nilp(x) ((x->type==BEL_SYMBOL)&&(x->sym==BEL_NIL))
-
-// TODO: Check for errors
-
 Bel*
 bel_car(Bel *p)
 {
+    if(!bel_pairp(p)) {
+        return bel_mkerror(
+            bel_mkstring("Cannot extract the car of ~a."),
+            bel_mkpair(p, bel_g_nil));
+    }
+    
     if(bel_nilp(p))
         return bel_g_nil;
     return p->pair->car;
@@ -161,6 +211,12 @@ bel_car(Bel *p)
 Bel*
 bel_cdr(Bel *p)
 {
+    if(!bel_pairp(p)) {
+        return bel_mkerror(
+            bel_mkstring("Cannot extract the cdr of ~a."),
+            bel_mkpair(p, bel_g_nil));
+    }
+    
     if(bel_nilp(p))
         return bel_g_nil;
     return p->pair->cdr;
@@ -192,15 +248,40 @@ bel_mkchar(Bel_char c)
 Bel*
 bel_char_from_binary(Bel *list)
 {
-    size_t len = bel_length(list);
-    Bel_char mask = '\0';
+    if(!bel_pairp(list)) {
+        return bel_mkerror(
+            bel_mkstring("The binary representation of "
+                         "a character must be a string of "
+                         "characters \\0 and \\1."),
+            bel_g_nil);
+    }
 
-    // TODO: This does not check the type and contents
-    // of the list properly!
+    size_t len = bel_length(list);
+
+    if(len != 8) {
+        return bel_mkerror(
+            bel_mkstring("The binary representation of "
+                         "a character must have exactly "
+                         "eight characters \\0 or \\1."),
+            bel_g_nil);
+    }
+    
+    Bel_char mask = '\0';
     size_t i;
     Bel *current = list;
+    
     for(i = 0; i < len; i++) {
         Bel *bitchar = bel_car(current);
+
+        if(!bel_charp(bitchar)) {
+            return bel_mkerror(
+                bel_mkstring("The provided binary "
+                             "representation of a "
+                             "character does not contain "
+                             "only characters."),
+                bel_g_nil);
+        }
+        
         if(bitchar->chr == '1') {
             mask |= (1 << (7 - i));
         }
@@ -239,6 +320,14 @@ bel_mkstring(const char *str)
 char*
 bel_cstring(Bel *belstr)
 {
+    if(!bel_pairp(belstr)) {
+        puts("INTERNAL ERROR on bel_cstring: "
+             "argument is not a pair");
+        return NULL;
+    }
+    
+    // TODO: missing a decisive check for whether
+    // it really is a Bel string
     uint64_t len = bel_length(belstr);
     if(len == 0) return NULL;
     
@@ -246,6 +335,7 @@ bel_cstring(Bel *belstr)
 
     Bel *itr     = belstr;
     size_t i     = 0;
+    // TODO: may cause infinite loop...
     while(!bel_nilp(itr)) {
         str[i] = bel_car(itr)->chr;
         itr    = bel_cdr(itr);
@@ -261,7 +351,12 @@ bel_mkstream(const char* name, BEL_STREAM_STATUS status)
     Bel *ret           = GC_MALLOC(sizeof *ret);
     ret->type          = BEL_STREAM;
 
-    // TODO: Do not allow creating a closed stream
+    if(status == BEL_STREAM_CLOSED) {
+        return bel_mkerror(
+            bel_mkstring("Cannot create a stream with "
+                         "CLOSED status."),
+            bel_g_nil);
+    }
 
     if(!strncmp(name, "ins", 3)) {
         ret->stream.raw_stream = stdin;
@@ -273,8 +368,10 @@ bel_mkstream(const char* name, BEL_STREAM_STATUS status)
                   status == BEL_STREAM_READ ? "rb" : "wb");
         
         if(!ret->stream.raw_stream) {
-            // TODO: Propagate a proper error
-            return bel_g_nil;
+            return bel_mkerror(
+                bel_mkstring("Unable to open stream ~a."),
+                bel_mkpair(
+                    bel_mkstring(name), bel_g_nil));
         }
     }
 
@@ -298,11 +395,19 @@ bel_stream_dump_cache(Bel_stream *stream)
 Bel*
 bel_stream_write_bit(Bel_stream *stream, Bel_char bit)
 {
-    // Return nil iff bit is incorrect and stream is
-    // not at write status
-    if(stream->status != BEL_STREAM_WRITE
-       || (bit != '0' && bit != '1'))
-        return bel_g_nil;
+    if(bit != '0' || bit != '1') {
+        return bel_mkerror(
+            bel_mkstring("Written bit must be represented "
+                         "as a character 0 or 1"),
+            bel_g_nil);
+    }
+
+    if(stream->status != BEL_STREAM_WRITE) {
+        return bel_mkerror(
+            bel_mkstring("Write stream is not at WRITE "
+                         "state"),
+            bel_g_nil);
+    }
 
     if(stream->cache_used >= 8) {
         return bel_stream_dump_cache(stream);
@@ -313,13 +418,14 @@ bel_stream_write_bit(Bel_stream *stream, Bel_char bit)
         stream->cache_used++;
     }
     
-    return bel_g_t;
+    return bel_mkchar(bit);
 }
 
 Bel*
 bel_stream_fill_cache(Bel_stream *stream)
 {
     if(!fread(&stream->cache, 1, 1, stream->raw_stream)) {
+        // Return nil on EOF
         return bel_g_nil;
     }
     stream->cache_used = 8;
@@ -329,14 +435,19 @@ bel_stream_fill_cache(Bel_stream *stream)
 Bel*
 bel_stream_read_bit(Bel_stream *stream)
 {
-    // Return nil iff stream is not at read status
-    if(stream->status != BEL_STREAM_READ)
-        return bel_g_nil;
+    if(stream->status != BEL_STREAM_READ) {
+        return bel_mkerror(
+            bel_mkstring("Read stream is not at READ "
+                         "state"),
+            bel_g_nil);
+    }
     
     Bel *ret;
     if(stream->cache_used == 0) {
         ret = bel_stream_fill_cache(stream);
-        if(bel_nilp(ret)) return ret;
+        if(bel_nilp(ret)) {
+            return bel_mksymbol("eof");
+        }
     }
 
     uint8_t mask = (1 << (stream->cache_used - 1));
@@ -349,12 +460,17 @@ bel_stream_read_bit(Bel_stream *stream)
 Bel*
 bel_stream_close(Bel *obj)
 {
-    // TODO: Check for errors
-    if(obj->type != BEL_STREAM
-        || obj->stream.status == BEL_STREAM_CLOSED) {
-        // TODO: Returning nil here, but should
-        // signal an error
-        return bel_g_nil;
+    if(obj->type != BEL_STREAM) {
+        return bel_mkerror(
+            bel_mkstring("Cannot close something that "
+                         "is not a stream."),
+            bel_g_nil);
+    }
+    
+    if(obj->stream.status == BEL_STREAM_CLOSED) {
+        return bel_mkerror(
+            bel_mkstring("Cannot close a closed stream."),
+            bel_g_nil);
     }
 
     // Dump cache before closing
@@ -368,9 +484,11 @@ bel_stream_close(Bel *obj)
         return bel_g_t;
     }
 
-    // TODO: Should signal error on stream closing
-    // failure as well
-    return bel_g_nil;
+    return bel_mkerror(
+        bel_mkstring("Error closing stream: ~a."),
+        bel_mkpair(
+            bel_mkstring(strerror(errno)),
+            bel_g_nil));
 }
 
 void
@@ -380,6 +498,16 @@ bel_init_streams(void)
     bel_g_outs     = bel_g_nil;
     bel_g_ins_sys  = bel_mkstream("ins",  BEL_STREAM_READ);
     bel_g_outs_sys = bel_mkstream("outs", BEL_STREAM_WRITE);
+}
+
+Bel*
+bel_mkerror(Bel *format, Bel *arglist)
+{
+    return bel_mkpair(
+        bel_mksymbol("lit"),
+        bel_mkpair(
+            bel_mksymbol("err"),
+            bel_mkpair(format, arglist)));
 }
 
 void
@@ -675,6 +803,20 @@ read_file_test()
 }
 
 void
+show_errors_test()
+{
+    Bel *err;
+    
+    // Unexisting file
+    err = bel_mkstream("waddawaddawadda", BEL_STREAM_READ);
+    bel_dbg_print(err);
+    putchar(10);
+
+    printf("Is this an error? %c\n",
+           bel_errorp(err) ? 'y' : 'n');
+}
+
+void
 run_tests()
 {
     puts("-- Running debug tests");
@@ -690,6 +832,8 @@ run_tests()
     character_list_test();
     puts("  -- Read five bytes from Believe's source");
     read_file_test();
+    puts("  -- Show a few errors on screen");
+    show_errors_test();
 }
 
 int
